f3af236adc3a5d7861533ab55b4528694eed5124
Alex <atlexeide@gmail.com>
2025-03-19 13:40:01 +0100

    Add paths

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
+import Signup from "./Components/Signup.jsx";
+import Login from "./Components/Login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
+        <Route exact path="login" element={<Login />} />
+        <Route exact path="signup" element={<Signup />} />
       </Routes>
     </Router>
   </StrictMode>
