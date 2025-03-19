3b11294899f6d6890e2f51f6dba8e5a0001190d3
Alex <atlexeide@gmail.com>
2025-03-19 11:28:40 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AskForAccont from "./Components/AskForAccount";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   if (!isLoggedIn) return <AskForAccont />;
   return <h1>test</h1>;
 }
