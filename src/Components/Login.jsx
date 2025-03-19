92407cf149b5cd2bac5c72ec1e75c2c83ab428d2
Alex <atlexeide@gmail.com>
2025-03-19 14:25:10 +0100

    Add buttons

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <form id="login">
+      <h2>Log in</h2>
       <section id="login-inputs">
         <input type="email" name="email" id="email" placeholder="Email" />
         <input
@@ -9,6 +13,22 @@ export default function Login() {
           id="password"
           placeholder="Password"
         />
+      </section>
+      <section id="login-buttons">
+        <button
+          onClick={() => {
+            alert("LOGIN LODIC");
+          }}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          Dont have an account?
+        </button>
       </section>
     </form>
   );
