7224043b9d2d548c20e10c3a8d917b3657560cff
Alex <atlexeide@gmail.com>
2025-03-19 11:12:52 +0100

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import AskForAccont from "./Components/AskForAccount";
+
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  if (!isLoggedIn) return <AskForAccont />;
   return <h1>test</h1>;
 }
