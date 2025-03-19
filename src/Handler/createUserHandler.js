c70212707450c50bc0fd1c820a229b44f62050d6
Alexander Eide <atlexeide@gmail.com>
2025-03-19 20:55:32 +0100

    Add console log

--- src/Handler/createUserHandler.js
+++ src/Handler/createUserHandler.js
@@ -1,3 +1,4 @@
 export function createUser(userObject) {
   const sqlString = `INSERT INTO users (isOrganizer, username, firstname, surname, birthday, country, phone, email, password) VALUES (${userObject.isOrganizer}, ${userObject.username}, ${userObject.firstname}, ${userObject.surname}, ${userObject.dob}, ${userObject.country}, ${userObject.phone}, ${userObject.email},${userObject.password})`;
+  console.log(sqlString);
 }
