7d1f6560bc82d3af0b77a8b077644e9ed56dd23f
Alex <atlexeide@gmail.com>
2025-03-20 13:44:37 +0100

    Move files

--- src/Models/Event.js
+++
@@ -1,8 +0,0 @@
-export class Event {
-  constructor(startTime, endTime, name, desc) {
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.name = name;
-    this.desc = desc;
-  }
-}
