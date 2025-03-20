75dac73764e8d019ed2a7578d877f4cf75abfac7
Alex <atlexeide@gmail.com>
2025-03-20 13:44:15 +0100

    Move models to server folder

---
+++ server/Models/Event.js
@@ -0,0 +1,8 @@
+export class Event {
+  constructor(startTime, endTime, name, desc) {
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.name = name;
+    this.desc = desc;
+  }
+}
