338abf93232fd850fdd3385d43cbbcda235a9ab7
Alex <atlexeide@gmail.com>
2025-03-20 15:09:33 +0100

    Update endpoints

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import { createUser } from "./Handler/createUserHandler.js";
 // console.log(import.meta.env.DB_PATH);
 
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  res.send("Recieved!");
+app.post("/create/user", (req, res) => {
+  createUser(req, res);
 });
 app.get("/test", (req, res) => {
   console.log(req.body);
