557068deb1b03bc4f39b264effeac54eaaa0511d
Alex <atlexeide@gmail.com>
2025-03-21 20:30:47 +0100

    Remove unused code

--- server/Handler/createConventionHandler.js
+++ server/Handler/createConventionHandler.js
@@ -1,12 +1,6 @@
 import sqlite3 from "sqlite3";
 
 export async function createConvention(req, res) {
-  const sqlCreateParticipantTableString = `CREATE TABLE ${req.body.name
-    .toLowerCase()
-    .replace(" ", "")}_participants (
-                                            user_id INTEGER REFERENCES users (id),
-                                            convention_id INTEGER REFERENCES conventions (id) );`;
-
   const sqlString = `INSERT INTO conventions (
                             start_time,
                             end_time,
@@ -32,14 +26,8 @@ export async function createConvention(req, res) {
       res.send({ status: `ERROR OCCURED`, error: e.errno });
     }
     if (e === null) {
-      // db.run(sqlCreateParticipantTableString);
       res.send({ status: 200 });
     }
   });
   db.close();
 }
-
-// CREATE TABLE test (
-//     user_id       INTEGER REFERENCES users (id),
-//     convention_id INTEGER REFERENCES conventions (id)
-// );
