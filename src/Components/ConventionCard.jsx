ed85e4bf571034a5ee6a0c2811585876852c2359
Alex <atlexeide@gmail.com>
2025-03-22 00:28:24 +0100

    Add figure element

--- src/Components/ConventionCard.jsx
+++ src/Components/ConventionCard.jsx
@@ -1,3 +1,4 @@
+import { fetchLogo } from "../fetchLogo";
 import "./ConventionCard.css";
 
 export default function ConventionCard({ con }) {
@@ -6,11 +7,17 @@ export default function ConventionCard({ con }) {
       <h2>{con.convention_name}</h2>
       <section className="convention-info">
         <div>
+          <figure>
+            <img
+              src={con.logo_path ? fetchLogo(con.logo_path) : "/pawske.png"}
+              alt={`${con.convention_name} logo`}
+            />
+          </figure>
           <span>Starts: {con.start_time}</span>
           <br />
           <span>Ends: {con.end_time}</span>
         </div>
-        <h3>Convention desciption:</h3>
+        <h3>About convention:</h3>
         <p>{con.convention_description}</p>
       </section>
     </article>
