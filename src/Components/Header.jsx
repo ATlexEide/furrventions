3dadd66874dc4e4370df5273ce5c9ae954392892
Alex <atlexeide@gmail.com>
2025-03-22 00:50:11 +0100

    Add header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <figure>
+        <img src="" alt="" />
+      </figure>
+      <nav>
+        <button></button>
+        <button></button>
+        <button></button>
+      </nav>
+    </header>
+  );
+}
