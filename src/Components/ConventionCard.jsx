43d41cc003500e4c6a90df643307021d829f51ed
Alex <atlexeide@gmail.com>
2025-03-22 00:31:39 +0100

    Change logo placement

--- src/Components/ConventionCard.jsx
+++ src/Components/ConventionCard.jsx
@@ -4,15 +4,15 @@ import "./ConventionCard.css";
 export default function ConventionCard({ con }) {
   return (
     <article className="convention-card">
+      <figure className="convention-logo">
+        <img
+          src={con.logo_path ? fetchLogo(con.logo_path) : "/pawske.png"}
+          alt={`${con.convention_name} logo`}
+        />
+      </figure>
       <h2>{con.convention_name}</h2>
       <section className="convention-info">
         <div>
-          <figure>
-            <img
-              src={con.logo_path ? fetchLogo(con.logo_path) : "/pawske.png"}
-              alt={`${con.convention_name} logo`}
-            />
-          </figure>
           <span>Starts: {con.start_time}</span>
           <br />
           <span>Ends: {con.end_time}</span>
