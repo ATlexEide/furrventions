0a1b018d5d7343130e2652f9cd2f0b06bdc506da
Alex <atlexeide@gmail.com>
2025-03-22 06:50:17 +0100

    Add header logic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,33 @@
 import "./Header.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState({
+    id: 0,
+    username: "V",
+    isOrganizer: true,
+  });
+
   return (
     <header>
       <figure>
         <img src="" alt="" />
       </figure>
+      <Link to="/">
+        <h1>Hello {user.username}</h1>
+      </Link>
       <nav>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button>Lorem</button>
+        <button>ipsum</button>
+        <button>My Conventions</button>
+        {user.isOrganizer && (
+          <button onClick={() => navigate(`manage/${user.id}/conventions`)}>
+            Manage events
+          </button>
+        )}
       </nav>
     </header>
   );
