dc7d1b89b18b4f7c615ed35c2ae2fcd40a9862c0
Alex <atlexeide@gmail.com>
2025-03-22 06:49:33 +0100

    Add get handler

--- server/Handler/getConventionsHandler.js
+++ server/Handler/getConventionsHandler.js
@@ -18,3 +18,23 @@ export async function getAllConventions(req, res) {
   });
   db.close();
 }
+
+export async function getOrganizerConventions(req, res) {
+  console.log(req.params.id);
+  const sqlString = `SELECT * FROM conventions WHERE organizer_id = ${req.params.id};`;
+  // eslint-disable-next-line no-undef
+  const db = new sqlite3.Database(process.env.DB_PATH, sqlite3.OPEN_READWRITE);
+  db.all(sqlString, (e, rows) => {
+    if (e !== null) {
+      console.log(e);
+      console.log(e.errno);
+      res.send({
+        status: `ERROR OCCURED`,
+        error: e,
+        errorMessage: e.message,
+      });
+    }
+    if (e === null) res.send(rows);
+  });
+  db.close();
+}
