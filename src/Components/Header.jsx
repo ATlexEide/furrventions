0e4b7d843c05927ad12f17abb1a123a9105f006f
Alexander Eide <atlexeide@gmail.com>
2025-03-23 20:49:42 +0100

    Move userobject

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,33 +2,31 @@ import "./Header.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Header() {
+export default function Header({ _user }) {
   const navigate = useNavigate();
   // eslint-disable-next-line no-unused-vars
-  const [user, setUser] = useState({
-    id: 0,
-    username: "V",
-    isOrganizer: true,
-  });
+  const [user, setUser] = useState(_user);
 
-  return (
-    <header>
-      <figure>
-        <img src="" alt="" />
-      </figure>
-      <Link to="/">
-        <h1>Hello {user.username}</h1>
-      </Link>
-      <nav>
-        <button>Lorem</button>
-        <button>ipsum</button>
-        <button>My Conventions</button>
-        {user.isOrganizer && (
-          <button onClick={() => navigate(`manage/${user.id}/conventions`)}>
-            Manage events
-          </button>
-        )}
-      </nav>
-    </header>
-  );
+  if (user.username) {
+    return (
+      <header>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <Link to="/">
+          <h1>Hello {user.username}</h1>
+        </Link>
+        <nav>
+          <button>Lorem</button>
+          <button>ipsum</button>
+          <button>My Conventions</button>
+          {user.isOrganizer && (
+            <button onClick={() => navigate(`manage/${user.id}/conventions`)}>
+              Manage events
+            </button>
+          )}
+        </nav>
+      </header>
+    );
+  }
 }
