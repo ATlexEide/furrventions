c5f412641dcbea1527e4407a72d2d81da6e1a86f
Alexander Eide <atlexeide@gmail.com>
2025-03-24 23:12:57 +0100

    Update header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,44 @@
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
+  useUser,
 } from "@clerk/clerk-react";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { user } = useUser();
   return (
     <header>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
+        {user && (
+          <Link to="/">
+            <h1>
+              Hello,
+              {user.username.charAt(0).toUpperCase() + user.username.slice(1)}!
+            </h1>
+          </Link>
+        )}
+        <button
+          onClick={() => {
+            navigate("conventions/add");
+          }}
+        >
+          Add convention
+        </button>
+        <button
+          onClick={() => {
+            navigate("conventions");
+          }}
+        >
+          View conventions
+        </button>
         <UserButton />
       </SignedIn>
     </header>
