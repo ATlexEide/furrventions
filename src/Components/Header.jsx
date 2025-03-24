ed176ef6d01ff8f5811aba4e65dc7e6f2dd9dd70
Alexander Eide <atlexeide@gmail.com>
2025-03-24 16:21:51 +0100

    Import clerk

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,12 @@
 import "./Header.css";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 export default function Header({ _user }) {
   const navigate = useNavigate();
@@ -10,22 +16,12 @@ export default function Header({ _user }) {
   if (user.username) {
     return (
       <header>
-        <figure>
-          <img src="" alt="" />
-        </figure>
-        <Link to="/">
-          <h1>Hello {user.username}</h1>
-        </Link>
-        <nav>
-          <button>Lorem</button>
-          <button>ipsum</button>
-          <button>My Conventions</button>
-          {user.isOrganizer && (
-            <button onClick={() => navigate(`manage/${user.id}/conventions`)}>
-              Manage events
-            </button>
-          )}
-        </nav>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </header>
     );
   }
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,13 @@ import Login from "./Components/Login.jsx";
 import ConventionList from "./Components/ConventionList.jsx";
 import Header from "./Components/Header.jsx";
 import ManageConventions from "./Components/ManageConventions.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 const user = {
   id: 0,
@@ -17,19 +24,21 @@ const user = {
 };
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Header _user={user} />
-      <Routes>
-        <Route exact path="login" element={<Login />} />
-        <Route exact path="signup" element={<Signup />} />
-        <Route exact path="/" element={<App />} />
-        <Route
-          exact
-          path="manage/:id/conventions"
-          element={<ManageConventions />}
-        />
-        <Route exact path="conventions" element={<ConventionList />} />
-      </Routes>
-    </Router>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Router>
+        <Header _user={user} />
+        <Routes>
+          <Route exact path="login" element={<Login />} />
+          <Route exact path="signup" element={<Signup />} />
+          <Route exact path="/" element={<App />} />
+          <Route
+            exact
+            path="manage/:id/conventions"
+            element={<ManageConventions />}
+          />
+          <Route exact path="conventions" element={<ConventionList />} />
+        </Routes>
+      </Router>
+    </ClerkProvider>
   </StrictMode>
 );
