bd92310bc4c4f8ffd6c654fbbe9339f215b0e646
Alexander Eide <atlexeide@gmail.com>
2025-03-24 19:47:18 +0100

    Add Supabase integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,79 @@
-import { useState } from "react";
-import AskForAccont from "./Components/AskForAccount";
+"use client";
+import { useEffect, useState } from "react";
+import { useSession, useUser } from "@clerk/clerk-react";
+import { createClient } from "@supabase/supabase-js";
 
-import "./App.css";
+export default function Home() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
+  // The `useUser()` hook will be used to ensure that Clerk has loaded data about the logged in user
+  const { user } = useUser();
+  // The `useSession()` hook will be used to get the Clerk session object
+  const { session } = useSession();
 
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // Create a custom supabase client that injects the Clerk Supabase token into the request headers
+  function createClerkSupabaseClient() {
+    return createClient(
+      import.meta.env.VITE_SUPABASE_URL,
+      import.meta.env.VITE_SUPABASE_KEY,
+      {
+        global: {
+          // Get the custom Supabase token from Clerk
+          fetch: async (url, options = {}) => {
+            const clerkToken = await session?.getToken({
+              template: "supabase",
+            });
 
-  if (!isLoggedIn) return <AskForAccont setIsLoggedIn={setIsLoggedIn} />;
-  return <h1>test</h1>;
+            // Insert the Clerk Supabase token into the headers
+            const headers = new Headers(options?.headers);
+            headers.set("Authorization", `Bearer ${clerkToken}`);
+
+            // Now call the default fetch
+            return fetch(url, {
+              ...options,
+              headers,
+            });
+          },
+        },
+      }
+    );
+  }
+
+  // Create a `client` object for accessing Supabase data using the Clerk token
+  const client = createClerkSupabaseClient();
+  console.log(client);
+  // This `useEffect` will wait for the User object to be loaded before requesting
+  // the tasks for the logged in user
+  useEffect(() => {
+    if (!user) return;
+
+    async function loadTasks() {
+      setLoading(true);
+      const { data, error } = await client.from("users").select();
+      if (!error) setTasks(data);
+      setLoading(false);
+    }
+
+    loadTasks();
+  }, [user]);
+
+  // async function createTask(e) {
+  //   e.preventDefault();
+  //   // Insert task into the "tasks" database
+  //   await client.from("tasks").insert({
+  //     name,
+  //   });
+  //   window.location.reload();
+  // }
+
+  return (
+    <div>
+      <h1>TESTING</h1>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && <p>TEST</p>}
+    </div>
+  );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,4 @@
 import "./Header.css";
-import { useState } from "react";
 import {
   SignedIn,
   SignedOut,
@@ -7,20 +6,15 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 
-export default function Header({ _user }) {
-  // eslint-disable-next-line no-unused-vars
-  const [user, setUser] = useState(_user);
-
-  if (user.username) {
-    return (
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    );
-  }
+export default function Header() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
