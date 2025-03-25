49617e55d95bdd7d113778d2b534f25244f3774e
Alex <atlexeide@gmail.com>
2025-03-25 10:21:39 +0100

    Add form

--- src/Components/AddConvention.jsx
+++ src/Components/AddConvention.jsx
@@ -1,28 +1,130 @@
 import { useSupabase } from "../SupabaseHook";
 import { useUser } from "@clerk/clerk-react";
+import { useState } from "react";
 
 export default function AddConvention() {
   const { user } = useUser();
   const supabase = useSupabase();
+  const [conventionInfo, setConventionInfo] = useState({
+    organizerID: user.id,
+  });
+  function clearConventionInfo() {
+    setConventionInfo({
+      organizerID: user.id,
+      name: "",
+      description: "",
+      start_time: "",
+      end_time: "",
+      website: "",
+    });
+  }
   async function addConvention(obj) {
     const { data, error } = await supabase
       .from("conventions")
       .insert(obj)
       .select();
     if (error) console.log(error);
-    console.log(data);
+    else {
+      clearConventionInfo();
+      console.log(data);
+    }
   }
+
   return (
-    <button
-      onClick={() => {
-        addConvention({
-          name: "TEST NAME",
-          description: "TEST DESC",
-          organizerID: user.id,
-        });
-      }}
-    >
-      Add Demo
-    </button>
+    <>
+      <form id="signup">
+        <h2>Create an account</h2>
+        <section id="signup-inputs">
+          {/* CONVENTION NAME */}
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Convention Name"
+            value={conventionInfo.name}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setConventionInfo({
+                ...conventionInfo,
+                name: e.target.value,
+              });
+            }}
+          />
+          {/* CONVENTION DESCRIPTION */}
+          <input
+            type="text"
+            name="description"
+            id="description"
+            placeholder="Description"
+            value={conventionInfo.description}
+            onChange={(e) => {
+              setConventionInfo({
+                ...conventionInfo,
+                description: e.target.value,
+              });
+            }}
+          />
+
+          {/* START TIME */}
+          <label htmlFor="start-time">Convention starts:</label>
+          <input
+            type="date"
+            name="start-time"
+            id="start-time"
+            value={conventionInfo.start_time}
+            onChange={(e) => {
+              setConventionInfo({
+                ...conventionInfo,
+                start_time: e.target.value,
+              });
+            }}
+          />
+
+          {/* END TIME */}
+          <label htmlFor="start-time">Convention starts:</label>
+          <input
+            type="date"
+            name="end-time"
+            id="end-time"
+            value={conventionInfo.end_time}
+            onChange={(e) => {
+              setConventionInfo({
+                ...conventionInfo,
+                end_time: e.target.value,
+              });
+            }}
+          />
+
+          <input
+            type="text"
+            name="website"
+            id="website"
+            placeholder="Convention Website"
+            value={conventionInfo.website}
+            onChange={(e) => {
+              setConventionInfo({ ...conventionInfo, website: e.target.value });
+            }}
+          />
+        </section>
+        <section id="buttons">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              clearConventionInfo();
+            }}
+          >
+            Clear
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              addConvention(conventionInfo);
+            }}
+          >
+            Add
+          </button>
+        </section>
+      </form>
+    </>
   );
 }
