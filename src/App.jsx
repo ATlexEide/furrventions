9c0a42f0135c74f17d8262a2f6fcccfc46730723
Alex <atlexeide@gmail.com>
2025-03-25 12:29:53 +0100

    Update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 "use client";
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate();
   return (
     <>
       <SignedIn>
-        <h1>SIGNED IN</h1>
-        <button onClick={() => navigate("conventions")}>Test</button>
+        <h2>Welcome to {import.meta.env.VITE_APPNAME}</h2>
       </SignedIn>
       <SignedOut>
         <h1>Please log in to access site</h1>
