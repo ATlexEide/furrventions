208ebff9ddd25fb3516fcdd82881fbe1c72bf2b0
Alex <atlexeide@gmail.com>
2025-03-25 11:19:43 +0100

    Remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 "use client";
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
-// import { useUser } from "@clerk/clerk-react";
-import { useEffect /*useContext*/ } from "react";
 import { useNavigate } from "react-router-dom";
-// import { SupabaseContext } from "./SupabaseContext";
 
 export default function Home() {
   const navigate = useNavigate();
-  // const { supabase } = useContext(SupabaseContext);
-  // const { user } = useUser();
-  // async function taskTest() {
-  //   console.log("YIPP");
-  //   const { data, error } = await supabase
-  //     .from("tasks")
-  //     .insert({ name: "IM STUPID", user_id: user.id })
-  //     .select();
-  //   if (error) console.log(error);
-  //   console.log(data);
-  //   return data;
-  // }
-  // async function fetchConventions() {
-  //   const { data, error } = await supabase.from("conventions").select();
-  //   if (error) console.log(error);
-  //   console.log(data);
-  //   return data;
-  // }
-
-  useEffect(() => {
-    // fetchConventions();
-  }, []);
   return (
     <>
       <SignedIn>
