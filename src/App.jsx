2d34ad83ea28609ea5abdb6d39bfe06715f681b7
Alex <atlexeide@gmail.com>
2025-03-25 14:28:54 +0100

    Fix logged out styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 "use client";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 
 export default function Home() {
   return (
-    <>
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "48px",
+      }}
+    >
       <SignedIn>
         <h2>Welcome to {import.meta.env.VITE_APPNAME}</h2>
       </SignedIn>
       <SignedOut>
-        <h1>Please log in to access site</h1>
+        <h1>Please sign in to access site</h1>
+        <SignInButton style={{ width: "50%" }} />
       </SignedOut>
-    </>
+    </section>
   );
 }
