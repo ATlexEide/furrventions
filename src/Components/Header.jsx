5f63ad0807238869ddafbedd84cbeb98cb83137a
Alex <atlexeide@gmail.com>
2025-03-25 11:37:54 +0100

    Add link id

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,35 +12,38 @@ export default function Header() {
   const navigate = useNavigate();
   const { user } = useUser();
   return (
-    <header>
+    <>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
-        {user && (
-          <Link to="/">
-            <h1>
-              Hello,
-              {user.username.charAt(0).toUpperCase() + user.username.slice(1)}!
-            </h1>
-          </Link>
-        )}
-        <button
-          onClick={() => {
-            navigate("conventions/add");
-          }}
-        >
-          Add convention
-        </button>
-        <button
-          onClick={() => {
-            navigate("conventions");
-          }}
-        >
-          View conventions
-        </button>
-        <UserButton />
+        <header>
+          {user && (
+            <Link id="header-home" to="/">
+              <h1>
+                Hello,
+                {user.username.charAt(0).toUpperCase() + user.username.slice(1)}
+                !
+              </h1>
+            </Link>
+          )}
+          <button
+            onClick={() => {
+              navigate("conventions/add");
+            }}
+          >
+            Add convention
+          </button>
+          <button
+            onClick={() => {
+              navigate("conventions");
+            }}
+          >
+            View conventions
+          </button>
+          <UserButton />
+        </header>
       </SignedIn>
-    </header>
+    </>
   );
 }
