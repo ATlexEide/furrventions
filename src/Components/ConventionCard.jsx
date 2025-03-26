10bbe0e238cb9b7c816efbe63cc87d5dc8ca1f3f
Alexander Eide <atlexeide@gmail.com>
2025-03-26 18:31:07 +0100

    Update return

--- src/Components/ConventionCard.jsx
+++ src/Components/ConventionCard.jsx
@@ -2,6 +2,17 @@ import { fetchLogo } from "../fetchLogo";
 import "./ConventionCard.css";
 
 export default function ConventionCard({ con }) {
+  con = {
+    convention_name: "TEST CON",
+    convention_location: "BERGEN",
+    convention_description: "TEST DESCRIPTION",
+    logo_path: "https://pawskeparty.com/images/logotype%20textwhite.png",
+    start_time: "01-01-0101",
+    end_time: "02-02-0202",
+    total_spots: 200,
+    taken_spots: 69,
+  };
+
   return (
     <article className="convention-card">
       <figure className="convention-logo">
@@ -10,15 +21,39 @@ export default function ConventionCard({ con }) {
           alt={`${con.convention_name} logo`}
         />
       </figure>
-      <h2>{con.convention_name}</h2>
       <section className="convention-info">
-        <div>
-          <span>Starts: {con.start_time}</span>
-          <br />
-          <span>Ends: {con.end_time}</span>
-        </div>
-        <h3>About convention:</h3>
-        <p>{con.convention_description}</p>
+        {con.convention_name && (
+          <h2 className="convention-name">{con.convention_name}</h2>
+        )}
+        {con.convention_location && <p>{con.convention_location}</p>}
+        {con.location && <p>{con.location}</p>}
+        {con.total_spots && (
+          <p>
+            <span className="info-prefix">Total spots:</span> {con.total_spots}
+          </p>
+        )}
+        {con.taken_spots && (
+          <p>
+            <span className="info-prefix">Taken spots:</span> {con.taken_spots}
+          </p>
+        )}
+        {con.start_time && con.end_time && (
+          <div className="convention-dates">
+            <span>
+              <span className="info-prefix">Starts:</span> {con.start_time}
+            </span>
+            <br />
+            <span>
+              <span className="info-prefix">Ends:</span> {con.end_time}
+            </span>
+          </div>
+        )}
+        {con.convention_description && (
+          <div>
+            <h3 className="convention-desc-title">About convention:</h3>
+            <p className="convention-desc">{con.convention_description}</p>
+          </div>
+        )}
       </section>
     </article>
   );
