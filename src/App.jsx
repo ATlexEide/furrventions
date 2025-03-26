ce23c77bc63b73a2ab65c5daf3bba81e336138c0
Alexander Eide <atlexeide@gmail.com>
2025-03-26 22:35:24 +0100

    Add mascot (using e621 mascot temporarily)

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function Home() {
         <div>
           <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
           <h3>Your best friend in finding furry conventions!</h3>
+          <img id="mascot" src="mascot.png" alt="" />
         </div>
       </SignedIn>
       <SignedOut>
