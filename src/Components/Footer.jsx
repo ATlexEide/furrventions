dbd8cca5736f0d361ec702406b7d8f4352cfa6e1
Alexander Eide <atlexeide@gmail.com>
2025-03-26 21:51:27 +0100

    Update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,20 +4,28 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p>Developed by {import.meta.env.VITE_DEVHANDLE}</p>
-      <Link to="support">Support Me</Link> |
-      <ul id="socials">
-        <p>Socials //</p>
-        <li>
-          <a href="https://www.linkedin.com/in/alexander-eide/">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://github.com/ATlexEide">GitHub</a>
-        </li>
-        <li>
-          <a href="https://www.alexandereide.com/">Website</a>
-        </li>
-      </ul>
+      <div className="footer-part">
+        <p>Developed by {import.meta.env.VITE_DEVHANDLE}</p>
+      </div>{" "}
+      |
+      <div className="footer-part">
+        <ul id="socials">
+          <p>Socials //</p>
+          <li>
+            <a href="https://www.linkedin.com/in/alexander-eide/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/ATlexEide">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.alexandereide.com/">Website</a>
+          </li>
+        </ul>
+      </div>
+      |{" "}
+      <div className="footer-part">
+        <Link to="support">Support Me</Link>
+      </div>
     </footer>
   );
 }
