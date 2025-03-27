9edaecc8a6b04434195280da7fb1369a1219c582
Alexander Eide <atlexeide@gmail.com>
2025-03-28 00:52:33 +0100

    Add convention markers

--- src/Components/ConventionList.jsx
+++ src/Components/ConventionList.jsx
@@ -1,3 +1,13 @@
+// fetch(
+//   `https://maps.googleapis.com/maps/api/geocode/json?key=${
+//     import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+//   }&place_id=${con.location}`
+// )
+//   .then((res) => res.json())
+//   .then((res) =>
+//     setCordsArray([...cordsArray, res.results[0].geometry.location])
+//   );
+
 import {
   APILoader,
   PlacePicker
@@ -8,33 +18,49 @@ import { useSupabase } from "../SupabaseHook.jsx";
 import ConventionCard from "./ConventionCard";
 // import Map from "./Map.jsx";
 
-import { APIProvider, CollisionBehavior, Map } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  CollisionBehavior,
+  Map,
+  Marker
+} from "@vis.gl/react-google-maps";
 
 export default function ConventionList() {
   const supabase = useSupabase();
-  const countries = [];
-
   const [cons, setCons] = useState([]);
 
   const [filteredCons, setFilteredCons] = useState([]);
+
   const [nameSearchInput, setNameSearchInput] = useState("");
   const [sliderValue, setSliderValue] = useState(500);
   const [sliderUpdated, setSliderUpdated] = useState(false);
   const [mapOverviewEnabled, setMapOverviewEnabled] = useState(false);
   const [location, setLocation] = useState("");
-  console.log("location from list", location);
   const [loading, setLoading] = useState(true);
   const [refresh, setRefresh] = useState(false);
+
+  async function placeMarker(con, i) {
+    console.log("MARKER CON LOCATION", con.location);
+    const loc = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?key=${
+        import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+      }&place_id=${con.location}`
+    )
+      .then((res) => res.json())
+      .then((res) => res.results[0].geometry.location);
+    return <Marker key={i} position={loc} />;
+  }
+
   async function fetchConventions() {
     const { data, error } = await supabase.from("conventions").select();
     if (error) console.log(error);
     console.log("Fetched cons: ", data);
-    setCons(data.sort((a, b) => a.start_time - b.start_time));
+
+    setCons(data);
     setLoading(false);
     setRefresh(false);
   }
 
-  console.log(cons);
   useEffect(() => {
     setLoading(true);
     fetchConventions();
@@ -42,26 +68,21 @@ export default function ConventionList() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [refresh]);
   useEffect(() => {
-    console.log(cons[0]);
     setFilteredCons(
       cons
         .filter((con) => con.name.toLowerCase().includes(nameSearchInput))
         .filter((con) => con.location.toLowerCase().includes(location, 0))
         .filter((con) => con.spots_total <= sliderValue)
     );
-    console.log(nameSearchInput);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [nameSearchInput, location, sliderValue]);
 
   const hasCons = Boolean(cons.length);
-  console.log("SLIDER VAL:", sliderValue);
-
   const hasFilteredCons = Boolean(filteredCons.length);
   const filtersEnabled =
     Boolean(nameSearchInput.length) ||
     Boolean(location.length) ||
     sliderUpdated;
-  console.log("filters", filtersEnabled);
 
   return (
     <section id="convention-list-cont">
@@ -100,7 +121,7 @@ export default function ConventionList() {
                   />
                   <div className="container">
                     <PlacePicker
-                      country={countries}
+                      country={[]}
                       placeholder={"Enter a place to see its address"}
                       onPlaceChange={(e) => setLocation(e.target.value.id)}
                     />
@@ -147,17 +168,19 @@ export default function ConventionList() {
               setMapOverviewEnabled(!mapOverviewEnabled);
             }}
           ></button>
-          {mapOverviewEnabled && (
+          {mapOverviewEnabled && !refresh && (
             <APIProvider
               solutionChannel="GMP_devsite_samples_v3_rgmbasicmap"
               apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
             >
               <Map
                 defaultZoom={8}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+                defaultCenter={{ lat: 59.9138688, lng: 10.7522454 }}
                 gestureHandling={"greedy"}
                 disableDefaultUI={true}
-              />
+              >
+                {cons.map((con, i) => placeMarker(con, i))}
+              </Map>
             </APIProvider>
           )}
           <ul id="convention-list">
