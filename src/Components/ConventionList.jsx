eaf513f784a168ecb1317fd440acc92aaddf4b08
Alexander Eide <atlexeide@gmail.com>
2025-03-28 01:09:57 +0100

    .

--- src/Components/ConventionList.jsx
+++ src/Components/ConventionList.jsx
@@ -172,20 +172,21 @@ export default function ConventionList() {
 
           <ul id="convention-list">
             {mapOverviewEnabled && !refresh && (
-              <APIProvider
-                solutionChannel="GMP_devsite_samples_v3_rgmbasicmap"
-                apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-              >
-                <Map
-                  id="map"
-                  defaultZoom={8}
-                  defaultCenter={{ lat: 59.9138688, lng: 10.7522454 }}
-                  gestureHandling={"greedy"}
-                  disableDefaultUI={true}
+              <div id="map">
+                <APIProvider
+                  solutionChannel="GMP_devsite_samples_v3_rgmbasicmap"
+                  apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
                 >
-                  {cons.map((con, i) => placeMarker(con, i))}
-                </Map>
-              </APIProvider>
+                  <Map
+                    defaultZoom={8}
+                    defaultCenter={{ lat: 59.9138688, lng: 10.7522454 }}
+                    gestureHandling={"greedy"}
+                    disableDefaultUI={true}
+                  >
+                    {cons.map((con, i) => placeMarker(con, i))}
+                  </Map>
+                </APIProvider>
+              </div>
             )}
             {hasFilteredCons &&
               !mapOverviewEnabled &&
