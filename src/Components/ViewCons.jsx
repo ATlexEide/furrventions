65430e037cc915a6f4104f8c121f6ea8664a853e
Alex <atlexeide@gmail.com>
2025-03-28 14:45:03 +0100

    Update viewcons

--- src/Components/ViewCons.jsx
+++ src/Components/ViewCons.jsx
@@ -1,16 +1,35 @@
+import { useSupabase } from "../SupabaseHook.jsx";
 import { useEffect, useState } from "react";
 import Loading from "./Loading.jsx";
-import ConventionList from "./ConventionList.jsx";
-import { FetchCons } from "../utils/fetchCons.jsx";
+import ConventionCard from "./ConventionCard.jsx";
+import Filter from "./Filter.jsx";
+import "./ViewCons.css";
 
 export default function ViewCons() {
+  const supabase = useSupabase();
+  const [cons, setCons] = useState([]);
   const [loading, setLoading] = useState(true);
-
-  if (!cons) return <Loading />;
-  return (
-    <section id="convention-list-cont">
-      {loading && <Loading />}
-      {!loading && <ConventionList setLoading={setLoading} />}
-    </section>
-  );
+  async function fetchCons() {
+    const { data, err } = await supabase.from("conventions").select();
+    if (err) throw new Error(err);
+    setCons(await data);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchCons();
+  }, []);
+  console.log(cons);
+  if (loading) return <Loading />;
+  if (cons)
+    return (
+      <>
+        <Filter />
+        <section id="convention-list-cont">
+          {cons.map((con, i) => (
+            <ConventionCard con={con} key={i} />
+          ))}
+        </section>
+      </>
+    );
+  return <h1>test</h1>;
 }
