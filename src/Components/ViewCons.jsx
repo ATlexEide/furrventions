98d8fa83568d78af1dde2f5f3c51239ede1df3e8
Alex <atlexeide@gmail.com>
2025-03-28 15:18:44 +0100

    Add filter logic

--- src/Components/ViewCons.jsx
+++ src/Components/ViewCons.jsx
@@ -7,27 +7,52 @@ import "./ViewCons.css";
 
 export default function ViewCons() {
   const supabase = useSupabase();
-  const [cons, setCons] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState({});
+  const [cons, setCons] = useState([]);
+  const [filteredCons, setFilteredCons] = useState([]);
+
   async function fetchCons() {
     const { data, err } = await supabase.from("conventions").select();
     if (err) throw new Error(err);
     setCons(await data);
     setLoading(false);
   }
+
+  function filterCons() {
+    console.log("filter", filter);
+    let filtered = cons.slice();
+    if (filter.name)
+      filtered = filtered.filter((con) =>
+        con.name.toLowerCase().includes(filter.name.toLowerCase())
+      );
+    if (filter.location)
+      filtered = filtered.filter((con) => con.location === filter.location);
+    if (filter.spots_total)
+      filtered = filtered.filter(
+        (con) => con.spots_total === filter.spots_total
+      );
+
+    return filtered;
+  }
+
   useEffect(() => {
     fetchCons();
   }, []);
+  useEffect(() => {
+    setFilteredCons(filterCons());
+  }, [filter]);
   console.log(cons);
+  console.log("filterd", filteredCons);
   if (loading) return <Loading />;
   if (cons)
     return (
       <>
-        <Filter />
+        <Filter filter={filter} setFilter={setFilter} filterCons={filterCons} />
         <section id="convention-list-cont">
-          {cons.map((con, i) => (
-            <ConventionCard con={con} key={i} />
-          ))}
+          {filteredCons.length
+            ? filteredCons.map((con, i) => <ConventionCard con={con} key={i} />)
+            : cons.map((con, i) => <ConventionCard con={con} key={i} />)}
         </section>
       </>
     );
