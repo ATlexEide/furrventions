7ccef5a7e9939e24fa279dec4bdcc2fcef0c7e22
Alexander Eide <atlexeide@gmail.com>
2025-03-30 21:53:43 +0200

    Update route

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,7 @@ import {
 } from "@clerk/clerk-react";
 import Kofi from "./Components/Kofi.jsx";
 import "./Components/ViewCons.css";
+import ViewConInfo from "./Components/ViewConInfo.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
@@ -32,42 +33,42 @@ if (!import.meta.env.VITE_SUPABASE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ClerkProvider
-      touchSession={false}
-      publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl="/"
-      appearance={{ baseTheme: shadesOfPurple }}
-    >
-      <ClerkLoading>
-        <div>Clerk is loading</div>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <Router>
-          <Header />
-          <main>
-            <Routes>
-              <Route exact path="/" element={<App />} />
-              <Route exact path="signin" element={<RedirectToSignIn />} />
-              <Route
-                exact
-                path="manage/:id/conventions"
-                element={<ManageConventions />}
-              />
-              <Route exact path="conventions" element={<ViewCons />} />
-              <Route exact path="conventions/add" element={<AddConvention />} />
-              <Route exact path="conventions/:id" element={<AddConvention />} />
-              <Route
-                exact
-                path="user/:id/manage/conventions"
-                element={<AddConvention />}
-              />
-              <Route exact path="support" element={<Kofi />} />
-            </Routes>
-          </main>
-          <Footer />
-        </Router>
-      </ClerkLoaded>
-    </ClerkProvider>
-  </StrictMode>
+  // <StrictMode>
+  <ClerkProvider
+    touchSession={false}
+    publishableKey={PUBLISHABLE_KEY}
+    afterSignOutUrl="/"
+    appearance={{ baseTheme: shadesOfPurple }}
+  >
+    <ClerkLoading>
+      <div>Clerk is loading</div>
+    </ClerkLoading>
+    <ClerkLoaded>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route exact path="signin" element={<RedirectToSignIn />} />
+            <Route
+              exact
+              path="manage/:id/conventions"
+              element={<ManageConventions />}
+            />
+            <Route exact path="conventions" element={<ViewCons />} />
+            <Route exact path="conventions/add" element={<AddConvention />} />
+            <Route exact path="conventions/:id" element={<ViewConInfo />} />
+            <Route
+              exact
+              path="user/:id/manage/conventions"
+              element={<AddConvention />}
+            />
+            <Route exact path="support" element={<Kofi />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </ClerkLoaded>
+  </ClerkProvider>
+  // </StrictMode>
 );
