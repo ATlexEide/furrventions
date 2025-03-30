e65b9cf54db35066642451ef99148c8084ca8c50
Alexander Eide <atlexeide@gmail.com>
2025-03-30 21:56:06 +0200

    Remove unused code

--- src/Components/ManageConventions.jsx
+++ src/Components/ManageConventions.jsx
@@ -1,21 +1,5 @@
-import { useEffect, useState } from "react";
-
-import { useSupabase } from "../SupabaseHook";
-
 export default function ManageConventions() {
   console.log("YIPP");
-  const supabase = useSupabase();
-  const [cons, setCons] = useState();
-  async function fetchConventions() {
-    console.log("YIPP");
-    const { data, error } = await supabase.from("conventions").select();
-    if (error) console.log(error);
-    console.log(data);
-    return data;
-  }
-  console.log(cons);
-  useEffect(() => {
-    setCons(fetchConventions());
-  }, []);
+
   return <></>;
 }
