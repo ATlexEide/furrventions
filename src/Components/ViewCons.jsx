1dc06567fd321c842aa50832cba4531c83aae4bd
Alexander Eide <atlexeide@gmail.com>
2025-03-30 21:57:12 +0200

    Refactor (?)

--- src/Components/ViewCons.jsx
+++ src/Components/ViewCons.jsx
@@ -1,62 +1,68 @@
-import { useSupabase } from "../SupabaseHook.jsx";
-import { useEffect, useState } from "react";
-import Loading from "./Loading.jsx";
+import { useState } from "react";
 import ConventionCard from "./ConventionCard.jsx";
 import Filter from "./Filter.jsx";
 import "./ViewCons.css";
+import { useConsArray } from "../utils/useCons.js";
+import Loading from "./Loading.jsx";
 
 export default function ViewCons() {
-  const supabase = useSupabase();
-  const [loading, setLoading] = useState(true);
+  const [cons, loading] = useConsArray();
   const [filter, setFilter] = useState({});
-  const [cons, setCons] = useState([]);
   const [filteredCons, setFilteredCons] = useState([]);
 
-  async function fetchCons() {
-    const { data, err } = await supabase.from("conventions").select();
-    if (err) throw new Error(err);
-    setCons(await data);
-    setLoading(false);
+  async function formatFilteredLocation() {
+    await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?key=${
+        import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+      }&place_id=${filter.location}`
+    )
+      .then((res) => res.json())
+      .then((res) =>
+        setFilter({
+          ...filter,
+          location_formatted: res.results[0].formatted_address
+        })
+      );
   }
 
-  function filterCons() {
-    console.log("filter", filter);
+  async function filterCons() {
+    console.clear();
+
     let filtered = cons.slice();
     if (filter.name)
       filtered = filtered.filter((con) =>
         con.name.toLowerCase().includes(filter.name.toLowerCase())
       );
-    // FIXME: compare location formatted adress instead of id to be more flexible
+    // FIXME: location search not working
     if (filter.location)
-      filtered = filtered.filter((con) => con.location === filter.location);
+      filtered = filtered.filter((con) =>
+        con.location_formatted.match(filter.location.toLowerCase())
+      );
+    // FIXME END
     if (filter.spots_total)
       filtered = filtered.filter(
         (con) => con.spots_total === filter.spots_total
       );
-
-    return filtered;
+    return await setFilteredCons(filtered);
   }
 
-  useEffect(() => {
-    fetchCons();
-  }, []);
-  useEffect(() => {
-    setFilteredCons(filterCons());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter]);
-  console.log(cons[0]);
+  console.log(cons);
+  // useEffect(() => {
+  //   filterCons();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
   console.log("filtered", filteredCons);
-  if (loading) return <Loading />;
-  if (cons)
-    return (
-      <>
-        <Filter filter={filter} setFilter={setFilter} filterCons={filterCons} />
-        <section id="convention-list-cont">
-          {filteredCons.length
-            ? filteredCons.map((con, i) => <ConventionCard con={con} key={i} />)
-            : cons.map((con, i) => <ConventionCard con={con} key={i} />)}
-        </section>
-      </>
-    );
-  return <h1>test</h1>;
+  console.log("filter", filter);
+  if (!cons) return <Loading />;
+  return (
+    <>
+      <Filter filter={filter} setFilter={setFilter} filterCons={filterCons} />
+      <section id="convention-list-cont">
+        {filteredCons.length
+          ? filteredCons.map((con, i) => <ConventionCard con={con} key={i} />)
+          : cons.map((con, i) => <ConventionCard con={con} key={i} />)}
+      </section>
+    </>
+  );
 }
