38f2882a7fe7b143201c5d56a2ee066b17cd2ab4
Alex <atlexeide@gmail.com>
2025-03-31 12:28:37 +0200

    Comment out unused code

--- src/Components/ViewCons.jsx
+++ src/Components/ViewCons.jsx
@@ -11,22 +11,22 @@ export default function ViewCons() {
   const [hasFilter, setHasFilter] = useState(false);
   const [filteredCons, setFilteredCons] = useState([]);
 
-  async function formatFilteredLocation() {
-    console.log("YAY");
-    await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?key=${
-        import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-      }&place_id=${filter.location}`
-    )
-      .then((res) => res.json())
-      .then((res) => console.log(res))
-      .then((res) =>
-        setFilter({
-          ...filter,
-          location_formatted: res.results[0].formatted_address
-        })
-      );
-  }
+  // async function formatFilteredLocation() {
+  //   console.log("YAY");
+  //   await fetch(
+  //     `https://maps.googleapis.com/maps/api/geocode/json?key=${
+  //       import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+  //     }&place_id=${filter.location}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => console.log(res))
+  //     .then((res) =>
+  //       setFilter({
+  //         ...filter,
+  //         location_formatted: res.results[0].formatted_address
+  //       })
+  //     );
+  // }
 
   async function filterCons() {
     console.log(cons[1].name.toLowerCase());
