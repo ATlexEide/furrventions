aefb2a6cc9c174b47c673b43f67f464e75566e6c
Alex <atlexeide@gmail.com>
2025-03-31 11:15:14 +0200

    Update filter logic

--- src/Components/ViewCons.jsx
+++ src/Components/ViewCons.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ConventionCard from "./ConventionCard.jsx";
 import Filter from "./Filter.jsx";
 import "./ViewCons.css";
@@ -7,16 +7,19 @@ import Loading from "./Loading.jsx";
 
 export default function ViewCons() {
   const [cons, loading] = useConsArray();
-  const [filter, setFilter] = useState({});
+  const [filter, setFilter] = useState(null);
+  const [hasFilter, setHasFilter] = useState(false);
   const [filteredCons, setFilteredCons] = useState([]);
 
   async function formatFilteredLocation() {
+    console.log("YAY");
     await fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?key=${
         import.meta.env.VITE_GOOGLE_MAPS_API_KEY
       }&place_id=${filter.location}`
     )
       .then((res) => res.json())
+      .then((res) => console.log(res))
       .then((res) =>
         setFilter({
           ...filter,
@@ -26,18 +29,24 @@ export default function ViewCons() {
   }
 
   async function filterCons() {
-    console.clear();
-
+    console.log(cons[1].location_formatted.toLowerCase());
+    console.log(filter.location.toLowerCase());
     let filtered = cons.slice();
+    setHasFilter(true);
     if (filter.name)
-      filtered = filtered.filter((con) =>
-        con.name.toLowerCase().includes(filter.name.toLowerCase())
-      );
+      filter.name === ""
+        ? setFilteredCons(cons)
+        : (filtered = filtered.filter((con) =>
+            con.name.toLowerCase().includes(filter.name.toLowerCase())
+          ));
     // FIXME: location search not working
-    if (filter.location)
+    if (filter.location) {
+      const regex = new RegExp(`.*${filter.location.toLowerCase()}.*`);
+      console.log(regex);
       filtered = filtered.filter((con) =>
-        con.location_formatted.match(filter.location.toLowerCase())
+        regex.test(con.location_formatted.toLowerCase())
       );
+    }
     // FIXME END
     if (filter.spots_total)
       filtered = filtered.filter(
@@ -48,12 +57,42 @@ export default function ViewCons() {
 
   console.log(cons);
 
+  useEffect(() => {
+    if (!filter) return;
+    if (!filter.name && !filter.location && !filter.spots_total) {
+      setHasFilter(false);
+      setFilter(null);
+      setFilteredCons([]);
+      return;
+    }
+    filterCons();
+  }, [filter]);
+
   console.log("filtered", filteredCons);
   console.log("filter", filter);
   if (!cons) return <Loading />;
+  if (hasFilter)
+    return (
+      <>
+        <Filter
+          setHasFilter={setHasFilter}
+          filter={filter}
+          setFilter={setFilter}
+          filterCons={filterCons}
+        />
+        {filteredCons.length &&
+          filteredCons.map((con, i) => <ConventionCard con={con} key={i} />)}
+        <h1>test</h1>
+      </>
+    );
   return (
     <>
-      <Filter filter={filter} setFilter={setFilter} filterCons={filterCons} />
+      <Filter
+        setHasFilter={setHasFilter}
+        filter={filter}
+        setFilter={setFilter}
+        filterCons={filterCons}
+      />
       <section id="convention-list-cont">
         {filteredCons.length
           ? filteredCons.map((con, i) => <ConventionCard con={con} key={i} />)
