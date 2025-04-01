17614b223ac7ddb646ce4ad5b8da58bac472d357
Alex <atlexeide@gmail.com>
2025-04-01 14:23:10 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
@@ -16,7 +15,6 @@ if (!import.meta.env.VITE_SUPABASE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
   <>
     <header>
       <figure id="logo-container">
@@ -26,6 +24,4 @@ createRoot(document.getElementById("root")).render(
     </header>
     <App />
   </>
-
-  // </StrictMode>
 );
