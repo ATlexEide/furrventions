5c0ff0e7e8b325cf95ba64981378a6ae5603f4b2
Alex <atlexeide@gmail.com>
2025-04-01 11:03:41 +0200

    Add random taglines

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@
 import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 
 export default function Home() {
-  console.log("yipp");
+  // Taglines for display under landing page greeting
+  const taglines = [
+    "Your best friend in finding furry conventions!",
+    "Yippieee",
+    "Finding meets and conventions made simple!"
+  ];
+
   return (
     <section id="catchphrase-cont">
       <SignedIn>
         <div>
           <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
-          <h3>Your best friend in finding furry conventions!</h3>
+          <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
         </div>
         <img id="mascot" src="mascot.png" alt="" />
       </SignedIn>
