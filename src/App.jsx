0148d86e786d1ab3d6f2d9446bb69273b0372785
Alexander Eide <atlexeide@gmail.com>
2025-04-03 17:35:14 +0200

    Remove different loggedin/loggedout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 "use client";
-import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
-
 export default function Home() {
   // Taglines for display under landing page greeting
   const taglines = [
@@ -11,17 +9,11 @@ export default function Home() {
 
   return (
     <section id="catchphrase-cont">
-      <SignedIn>
-        <div>
-          <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
-          <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
-        </div>
-        <img id="mascot" src="mascot.png" alt="" />
-      </SignedIn>
-      <SignedOut>
-        <h2 id="catchphrase">Please sign in to access site</h2>
-        <SignInButton style={{ width: "50%" }} />
-      </SignedOut>
+      <div>
+        <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
+        <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
+      </div>
+      <img id="mascot" src="mascot.png" alt="" />
     </section>
   );
 }
