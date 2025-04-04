757b5201cafa7fc13017c3ec21da62aa6e0fd56f
Alexander Eide <atlexeide@gmail.com>
2025-04-04 23:46:41 +0200

    Remove clerk logic

--- src/utils/useSupabase.jsx
+++ src/utils/useSupabase.jsx
@@ -1,33 +1,11 @@
 import { useState, useEffect } from "react";
-import { useSession } from "@clerk/clerk-react";
 import { createClient } from "@supabase/supabase-js";
 
 export function useSupabase() {
-  const { session } = useSession();
   const [supabase] = useState(() => {
     return createClient(
       import.meta.env.VITE_SUPABASE_URL,
-      import.meta.env.VITE_SUPABASE_KEY,
-      {
-        global: {
-          // Get the custom Supabase token from Clerk
-          fetch: async (url, options = {}) => {
-            const clerkToken = await session?.getToken({
-              template: "supabase"
-            });
-
-            // Insert the Clerk Supabase token into the headers
-            const headers = new Headers(options?.headers);
-            headers.set("Authorization", `Bearer ${clerkToken}`);
-
-            // Now call the default fetch
-            return fetch(url, {
-              ...options,
-              headers
-            });
-          }
-        }
-      }
+      import.meta.env.VITE_SUPABASE_KEY
     );
   });
   useEffect(() => {
@@ -38,8 +16,6 @@ export function useSupabase() {
         // supabase.auth.setAuth(clerkToken);
       }
     };
-
-    if (session) updateAuthToken();
-  }, [session]);
+  }, []);
   return supabase;
 }
