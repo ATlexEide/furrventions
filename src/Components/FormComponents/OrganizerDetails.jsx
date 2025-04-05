09b651e1da85b4b23998704dd4834e5480931f00
Alexander Eide <atlexeide@gmail.com>
2025-04-05 19:30:51 +0200

    Add logic

--- src/Components/FormComponents/OrganizerDetails.jsx
+++ src/Components/FormComponents/OrganizerDetails.jsx
@@ -1,17 +1,55 @@
-export default function OrganizerDetails() {
+export default function OrganizerDetails({
+  setPage,
+  page,
+  eventInfo,
+  setEventInfo
+}) {
   return (
-    <>
+    <div id="organizer-details">
       <label htmlFor="name">Organizer name</label>
-      <input id="name" type="text" />
+      <input
+        id="name"
+        type="text"
+        onChange={(e) => {
+          setEventInfo({ ...eventInfo, organizerName: e.target.value });
+        }}
+      />
 
       <label htmlFor="email">Support Email</label>
-      <input id="email" type="email" />
+      <input
+        id="email"
+        type="email"
+        onChange={(e) => {
+          setEventInfo({ ...eventInfo, organizerEmail: e.target.value });
+        }}
+      />
 
       <label htmlFor="phone">Phone</label>
-      <input id="phone" type="tel" />
+      <input
+        id="phone"
+        type="tel"
+        onChange={(e) => {
+          setEventInfo({ ...eventInfo, organizerPhone: e.target.value });
+        }}
+      />
 
       <label htmlFor="groupchat">Telegram/Discord</label>
-      <input id="groupchat" type="text" />
-    </>
+      <input
+        id="groupchat"
+        type="text"
+        onChange={(e) => {
+          setEventInfo({ ...eventInfo, organizerGroupChat: e.target.value });
+        }}
+      />
+      <button
+        className="red-btn"
+        onClick={(e) => {
+          e.preventDefault();
+          setPage(page + 1);
+        }}
+      >
+        I&apos;m not the organizer
+      </button>
+    </div>
   );
 }
