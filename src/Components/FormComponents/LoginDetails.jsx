44d6d7a3787797f00ce72b2b96b3979eb42200fc
Alexander Eide <atlexeide@gmail.com>
2025-04-07 00:16:33 +0200

    Add login details component

---
+++ src/Components/FormComponents/LoginDetails.jsx
@@ -0,0 +1,18 @@
+export default function LoginDetails() {
+  return (
+    <section id="add-login">
+      <label htmlFor="firstname">Email:</label>
+      <input
+        id="email"
+        type="email"
+        onChange={(e) => setTempUser({ ...tempUser, email: e.target.value })}
+      />{" "}
+      <label htmlFor="firstname">Password:</label>
+      <input
+        id="password"
+        type="password"
+        onChange={(e) => setTempUser({ ...tempUser, pw: e.target.value })}
+      />
+    </section>
+  );
+}
