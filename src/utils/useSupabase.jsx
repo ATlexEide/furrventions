49913d8b5bf7c8339971c173fe09a461f5bb54e8
Alex <atlexeide@gmail.com>
2025-04-07 10:53:47 +0200

    Update hook

--- src/utils/useSupabase.jsx
+++ src/utils/useSupabase.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-export function useSupabase() {
-  const [supabase] = useState(() => {
-    return createClient(
-      import.meta.env.VITE_SUPABASE_URL,
-      import.meta.env.VITE_SUPABASE_KEY
-    );
-  });
+export default function useSupabase() {
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY
+  );
+
   return supabase;
 }
