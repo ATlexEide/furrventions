d553844c9871663045f424d50937ac00e5493742
Alexander Eide <atlexeide@gmail.com>
2025-04-07 08:15:05 +0200

    Update userbutton

--- src/Components/UserButton.jsx
+++ src/Components/UserButton.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export default function UserButton() {
-  return <h1>test</h1>;
+  const [isOpen, setIsOpen] = useState();
+  const _width = 200;
+  return (
+    <>
+      {" "}
+      <button
+        style={{ width: `${_width}px` }}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        Manage profile
+      </button>
+      {isOpen && <h1>test</h1>}
+    </>
+  );
 }
