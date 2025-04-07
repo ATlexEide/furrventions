d56b98c9fe5dcfd348897b5ce9f72f01ead9b134
Alex <atlexeide@gmail.com>
2025-04-07 12:19:34 +0200

    Update signin route

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import Kofi from "./Components/Kofi.jsx";
 import "./styles/ViewCons.css";
 import ViewConInfo from "./Components/ViewConInfo.jsx";
 import SignUp from "./Components/Signup.jsx";
+import SignIn from "./Components/SignIn.jsx";
 
 if (!import.meta.env.VITE_SUPABASE_URL) {
   throw new Error("Missing Supabase Url");
@@ -36,7 +37,7 @@ createRoot(document.getElementById("root")).render(
       <main>
         <Routes>
           <Route exact path="/" element={<App supabase={supabase} />} />
-          <Route exact path="signin" />
+          <Route exact path="signin" element={<SignIn supabase={supabase} />} />
           <Route exact path="signup" element={<SignUp supabase={supabase} />} />
 
           <Route
