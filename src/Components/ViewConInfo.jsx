044bb0acd8b0b469e7edeaba5c9f8844c09d8335
Alex <atlexeide@gmail.com>
2025-04-07 14:23:09 +0200

    Comment out unused code

--- src/Components/ViewConInfo.jsx
+++ src/Components/ViewConInfo.jsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom";
 import ConventionCard from "./ConventionCard";
-import { useConsObject } from "../utils/useCons.jsx";
+// import { useConsObject } from "../utils/useCons.jsx";
 import Loading from "./Loading.jsx";
 
 export default function ViewConInfo() {
-  const [cons, loading] = useConsObject();
+  // const [cons, loading] = useConsObject();
   const params = useParams();
   const id = params.id;
   if (loading) return <Loading />;
--- src/utils/useCons.jsx
+++ src/utils/useCons.jsx
@@ -1,55 +1,55 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-function useFetchCons() {
-  const supabase = useSupabase();
-  console.log("Fetching cons");
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(
-    localStorage.getItem("conventions")
-      ? JSON.parse(localStorage.getItem("conventions"))
-      : null
-  );
-  async function fetch() {
-    const { data, err } = await supabase.from("conventions").select();
-    if (err) throw new Error(err);
-    localStorage.setItem("conventions", JSON.stringify(data));
-    setData(data);
-    setLoading(false);
-  }
-  useEffect(() => {
-    if (data) return;
-    fetch();
-  }, []);
-  return [data, loading];
-}
+// function useFetchCons() {
+//   const supabase = useSupabase();
+//   console.log("Fetching cons");
+//   const [loading, setLoading] = useState(false);
+//   const [data, setData] = useState(
+//     localStorage.getItem("conventions")
+//       ? JSON.parse(localStorage.getItem("conventions"))
+//       : null
+//   );
+//   async function fetch() {
+//     const { data, err } = await supabase.from("conventions").select();
+//     if (err) throw new Error(err);
+//     localStorage.setItem("conventions", JSON.stringify(data));
+//     setData(data);
+//     setLoading(false);
+//   }
+//   useEffect(() => {
+//     if (data) return;
+//     fetch();
+//   }, []);
+//   return [data, loading];
+// }
 
-export function useConsArray() {
-  const [data, loading] = useFetchCons();
-  const [consArray, setConsArray] = useState(data);
-  useEffect(() => {
-    if (!data) return;
-    setConsArray(data);
-  }, [data]);
-  return [consArray, loading];
-}
+// export function useConsArray() {
+//   const [data, loading] = useFetchCons();
+//   const [consArray, setConsArray] = useState(data);
+//   useEffect(() => {
+//     if (!data) return;
+//     setConsArray(data);
+//   }, [data]);
+//   return [consArray, loading];
+// }
 
-export function useConsObject() {
-  const [data, loading] = useFetchCons();
-  const [consObject, setConsObject] = useState(null);
+// export function useConsObject() {
+//   const [data, loading] = useFetchCons();
+//   const [consObject, setConsObject] = useState(null);
 
-  function createConventionObject() {
-    if (!data) return;
-    let _consObject = {};
-    data.map((con) => {
-      _consObject[con.id] = con;
-    });
-    setConsObject(_consObject);
-  }
+//   function createConventionObject() {
+//     if (!data) return;
+//     let _consObject = {};
+//     data.map((con) => {
+//       _consObject[con.id] = con;
+//     });
+//     setConsObject(_consObject);
+//   }
 
-  useEffect(() => {
-    createConventionObject();
-  }, [data]);
+//   useEffect(() => {
+//     createConventionObject();
+//   }, [data]);
 
-  console.log(consObject);
-  return [consObject, loading];
-}
+//   console.log(consObject);
+//   return [consObject, loading];
+// }
