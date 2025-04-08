61bbd101db568693983ac4519d77b3c1ed1085b6
Alex <atlexeide@gmail.com>
2025-04-08 12:21:12 +0200

    Update ManageConventions logic

--- src/Components/ManageConventions.jsx
+++ src/Components/ManageConventions.jsx
@@ -1,5 +1,36 @@
-export default function ManageConventions() {
-  console.log("YIPP");
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-  return <></>;
+export default function ManageConventions({ supabase }) {
+  const [test, setTest] = useState();
+  const { id } = useParams();
+  console.log("params id", id);
+
+  async function fetchUserConIds() {
+    // .eq("userID", "5d735e16-8b63-46e1-8c39-0993d96b78b6");
+    const { data, error } = await supabase
+      .from("participants")
+      .select(
+        `
+      conventions (
+        *
+      )
+    `
+      )
+      .eq("userID", id);
+    if (error) console.log(error);
+    if (data) setTest(data);
+  }
+  useEffect(() => {
+    if (!id) return;
+    fetchUserConIds();
+  }, [id]);
+
+  console.log(test);
+
+  return (
+    <ul>
+      {test && test.map((data, i) => <li key={i}>{data.conventions.name}</li>)}
+    </ul>
+  );
 }
