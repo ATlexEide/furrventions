19579ba23bcf88ae68726ccbe60c1269264cfb86
Alex <atlexeide@gmail.com>
2025-04-08 09:01:05 +0200

    Update account settings component

--- src/Components/UserButtonComponents/AccountSettings.jsx
+++ src/Components/UserButtonComponents/AccountSettings.jsx
@@ -1,3 +1,46 @@
-export default function AccountSettings() {
-  return <p>some settings</p>;
+import { useState } from "react";
+
+export default function AccountSettings({ user }) {
+  const [editingFurname, setEditingFurname] = useState(false);
+  const [editingEmail, setEditingEmail] = useState(false);
+  return (
+    <ul id="account-details">
+      <li>
+        <h4>
+          <strong>Username:</strong>
+        </h4>
+
+        <p>
+          {user.user_metadata.furname}
+          <button
+            className="edit-info-btn"
+            onClick={() => {
+              setEditingFurname(!editingFurname);
+            }}
+          >
+            ✎
+          </button>
+        </p>
+        {editingFurname && <input value={user.user_metadata.furname} />}
+      </li>
+
+      <li>
+        <h4>
+          <strong>Email:</strong>
+        </h4>
+        <p>
+          {user.user_metadata.email}
+          <button
+            className="edit-info-btn"
+            onClick={() => {
+              setEditingEmail(!editingEmail);
+            }}
+          >
+            ✎
+          </button>
+        </p>
+        {editingEmail && <input value={user.user_metadata.email} />}
+      </li>
+    </ul>
+  );
 }
