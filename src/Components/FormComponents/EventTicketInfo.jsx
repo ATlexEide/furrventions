18af099ca6685fb3d34b25db443dc64ebf7a2f7e
Alex <atlexeide@gmail.com>
2025-04-09 08:45:10 +0200

    Group label input and span

--- src/Components/FormComponents/EventTicketInfo.jsx
+++ src/Components/FormComponents/EventTicketInfo.jsx
@@ -1,9 +1,11 @@
 export default function EventTicketInfo({ eventInfo, setEventInfo }) {
   return (
     <>
-      <label htmlFor="price">Ticket price:</label>
-      <input id="price" type="number" />
-      <span>eur</span>
+      <div id="ticket-price">
+        <label htmlFor="price">Ticket price:</label>
+        <input id="price" type="number" />
+        <span>eur</span>
+      </div>
     </>
   );
 }
