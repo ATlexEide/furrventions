b52f835330f6c5357bc8b100505a531c9c01da41
Alexander Eide <atlexeide@gmail.com>
2025-04-09 07:03:13 +0200

    Group labels and inputs

--- src/Components/FormComponents/LoginDetails.jsx
+++ src/Components/FormComponents/LoginDetails.jsx
@@ -1,18 +1,22 @@
 export default function LoginDetails({ tempUser, setTempUser }) {
   return (
     <section id="add-login">
-      <label htmlFor="firstname">Email:</label>
-      <input
-        id="email"
-        type="email"
-        onChange={(e) => setTempUser({ ...tempUser, email: e.target.value })}
-      />{" "}
-      <label htmlFor="firstname">Password:</label>
-      <input
-        id="password"
-        type="password"
-        onChange={(e) => setTempUser({ ...tempUser, pw: e.target.value })}
-      />
+      <div className="input-container">
+        <label htmlFor="firstname">Email:</label>
+        <input
+          id="email"
+          type="email"
+          onChange={(e) => setTempUser({ ...tempUser, email: e.target.value })}
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="firstname">Password:</label>
+        <input
+          id="password"
+          type="password"
+          onChange={(e) => setTempUser({ ...tempUser, pw: e.target.value })}
+        />
+      </div>
     </section>
   );
 }
