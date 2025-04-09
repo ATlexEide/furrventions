a474ac082ef5556c491f4935cae7125336c998e9
Alexander Eide <atlexeide@gmail.com>
2025-04-09 07:04:11 +0200

    Group labels and inputs

--- src/Components/FormComponents/UserNames.jsx
+++ src/Components/FormComponents/UserNames.jsx
@@ -1,32 +1,40 @@
 export default function UserNames({ tempUser, setTempUser }) {
   return (
     <section id="add-names">
-      <label htmlFor="firstname">First name:</label>
-      <input
-        id="firstname"
-        type="text"
-        onChange={(e) =>
-          setTempUser({ ...tempUser, firstname: e.target.value })
-        }
-      />
-      <label htmlFor="firstname">Last name:</label>
-      <input
-        id="lastname"
-        type="text"
-        onChange={(e) => setTempUser({ ...tempUser, lastname: e.target.value })}
-      />
-      <label htmlFor="firstname">Furname (Username):</label>
-      <input
-        id="furname"
-        type="text"
-        onChange={(e) =>
-          setTempUser({
-            ...tempUser,
-            display_name: e.target.value,
-            furname: e.target.value
-          })
-        }
-      />
+      <div className="input-container">
+        <label htmlFor="firstname">First name:</label>
+        <input
+          id="firstname"
+          type="text"
+          onChange={(e) =>
+            setTempUser({ ...tempUser, firstname: e.target.value })
+          }
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="firstname">Last name:</label>
+        <input
+          id="lastname"
+          type="text"
+          onChange={(e) =>
+            setTempUser({ ...tempUser, lastname: e.target.value })
+          }
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="firstname">Furname (Username):</label>
+        <input
+          id="furname"
+          type="text"
+          onChange={(e) =>
+            setTempUser({
+              ...tempUser,
+              display_name: e.target.value,
+              furname: e.target.value
+            })
+          }
+        />
+      </div>
     </section>
   );
 }
