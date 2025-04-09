530247824a006834c924691ebbda149cab728d95
Alexander Eide <atlexeide@gmail.com>
2025-04-09 07:17:41 +0200

    Group label and input

--- src/Components/FormComponents/EventName.jsx
+++ src/Components/FormComponents/EventName.jsx
@@ -1,8 +1,10 @@
 export default function EventName({ eventInfo, setEventInfo }) {
   return (
     <>
-      <label htmlFor="name">Event name:</label>
-      <input id="name" type="text" />
+      <div className="input-container">
+        <label htmlFor="name">Event name:</label>
+        <input id="name" type="text" />
+      </div>
     </>
   );
 }
