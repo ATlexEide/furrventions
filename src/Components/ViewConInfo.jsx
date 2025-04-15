79bfc19ad0aa69f5d6889ee31e6add93ce157607
Alexander Eide <atlexeide@gmail.com>
2025-04-15 21:03:55 +0200

    Update component logic

--- src/Components/ViewConInfo.jsx
+++ src/Components/ViewConInfo.jsx
@@ -1,12 +1,43 @@
-import { useParams } from "react-router-dom";
-import ConventionCard from "./ConventionCard";
-// import { useConsObject } from "../utils/useCons.jsx";
-import Loading from "./Loading.jsx";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function ViewConInfo() {
-  // const [cons, loading] = useConsObject();
+export default function ViewConInfo({ supabase }) {
+  const navigate = useNavigate();
   const params = useParams();
   const id = params.id;
-  if (loading) return <Loading />;
-  return <ConventionCard con={cons[id]} />;
+  console.log(id);
+  const [con, setCon] = useState({});
+
+  async function fetchCon(id) {
+    const { data, error } = await supabase
+      .from("conventions")
+      .select()
+      .eq("id", id);
+    if (error) throw new Error("Fetching convention failed");
+    console.log(data);
+    setCon(data[0]);
+  }
+
+  if (con) console.log(con);
+
+  useEffect(() => {
+    if (!id) return;
+    fetchCon(id);
+  }, []);
+  // const [cons, loading] = useConsObject();
+
+  return (
+    <article>
+      <button
+        className="nav-button"
+        onClick={() => {
+          navigate("/conventions");
+        }}
+      >
+        🢀
+      </button>
+      <h1>{con.name}</h1>
+      <p>{con.description}</p>
+    </article>
+  );
 }
