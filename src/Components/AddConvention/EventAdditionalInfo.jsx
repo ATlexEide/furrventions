e2721d599ee499c245c64dc59a216d30875cff00
Alexander Eide <atlexeide@gmail.com>
2025-04-30 23:21:38 +0200

    Add inputs

--- src/Components/AddConvention/EventAdditionalInfo.jsx
+++ src/Components/AddConvention/EventAdditionalInfo.jsx
@@ -2,8 +2,32 @@ export default function EventAdditionalInfo({ eventInfo, setEventInfo }) {
   return (
     <>
       <div className="input-container">
-        <label htmlFor="name">Additional Info:</label>
-        <input id="name" type="text" />
+        <label htmlFor="website">Event website</label>
+        <input
+          id="website"
+          type="text"
+          onChange={(e) => {
+            setEventInfo({ ...eventInfo, website: e.target.value });
+          }}
+        />
+
+        <label htmlFor="name">Start time*</label>
+        <input
+          id="start-time"
+          type="datetime-local"
+          onChange={(e) => {
+            setEventInfo({ ...eventInfo, start_time: e.target.value });
+          }}
+        />
+
+        <label htmlFor="name">End time*</label>
+        <input
+          id="end-time"
+          type="datetime-local"
+          onChange={(e) => {
+            setEventInfo({ ...eventInfo, end_time: e.target.value });
+          }}
+        />
       </div>
     </>
   );
