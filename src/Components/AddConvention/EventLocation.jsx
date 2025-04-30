c09c82e8e6c2020564ea02857338eb0c90ce4c1e
Alexander Eide <atlexeide@gmail.com>
2025-04-30 23:21:45 +0200

    Add inputs

--- src/Components/AddConvention/EventLocation.jsx
+++ src/Components/AddConvention/EventLocation.jsx
@@ -1,9 +1,58 @@
+import { useState } from "react";
+
 export default function EventLocation({ eventInfo, setEventInfo }) {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+
+  async function fetchLocation() {
+    fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=json`)
+      .then((res) => res.json())
+      .then((res) => setResult(res));
+  }
   return (
     <>
       <div className="input-container">
-        <label htmlFor="location">Event location:</label>
-        <input id="location" type="text" />
+        <label htmlFor="location">Event location*</label>
+        <input
+          id="location"
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          //TODO: Add geocoding to select properly formatted location
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("rawrrr");
+            fetchLocation();
+          }}
+        >
+          Search
+        </button>
+        {result && (
+          <ul>
+            {result.map((res, i) => (
+              <li
+                key={i}
+                onClick={() => {
+                  console.log(res);
+                  setEventInfo({
+                    ...eventInfo,
+                    long: res.lon,
+                    lat: res.lat,
+                    location: res.display_name
+                  });
+                  setQuery(res.display_name);
+                  setResult(null);
+                }}
+              >
+                {res.display_name}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
