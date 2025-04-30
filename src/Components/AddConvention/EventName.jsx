5c7649733b24544d8d12d3a9f6b9e9aacf7b4816
Alexander Eide <atlexeide@gmail.com>
2025-04-30 22:29:25 +0200

    Add description input

--- src/Components/AddConvention/EventName.jsx
+++ src/Components/AddConvention/EventName.jsx
@@ -2,12 +2,20 @@ export default function EventName({ eventInfo, setEventInfo }) {
   return (
     <>
       <div className="input-container">
-        <label htmlFor="name">Event name:</label>
+        <label htmlFor="name">Event name*</label>
         <input
           id="name"
           type="text"
           onChange={(e) => {
             setEventInfo({ ...eventInfo, name: e.target.value });
+          }}
+        />
+        <label htmlFor="description">Event description*</label>
+        <input
+          id="description"
+          type="text"
+          onChange={(e) => {
+            setEventInfo({ ...eventInfo, description: e.target.value });
           }}
         />
       </div>
