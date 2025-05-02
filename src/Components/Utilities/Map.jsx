32fe15815d3308545f6299aeaaf66d288a014c44
Alexander Eide <atlexeide@gmail.com>
2025-05-02 20:00:53 +0200

    Add show all cons logic

--- src/Components/Utilities/Map.jsx
+++ src/Components/Utilities/Map.jsx
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Link } from "react-router-dom";
 
 function MapPlaceholder() {
   return (
@@ -10,19 +11,38 @@ function MapPlaceholder() {
 }
 
 export default function MapWithPlaceholder({
+  cons,
   conName = "Convention",
   cords = [60.39118002058307, 5.331024627773373]
 }) {
+  console.clear();
+  console.table(cons);
   return (
     <MapContainer
-      center={cords}
-      zoom={16}
+      center={cons ? [34.69039805875912, -35.210042531073285] : cords}
+      zoom={cons ? 3 : 16}
       scrollWheelZoom={true}
       placeholder={<MapPlaceholder />}
     >
-      <Marker draggable={false} position={cords} ref={null}>
-        <Popup minWidth={90}>{conName}</Popup>
-      </Marker>
+      {cons &&
+        cons.map((con, i) => (
+          <Marker
+            key={i}
+            draggable={false}
+            position={[con.lat, con.long]}
+            ref={null}
+          >
+            <Popup minWidth={90}>
+              {<Link to={`/conventions/${con.id}`}>{con.name}</Link>}
+            </Popup>
+          </Marker>
+        ))}
+
+      {!cons && (
+        <Marker draggable={false} position={cords} ref={null}>
+          <Popup minWidth={90}>{conName}</Popup>
+        </Marker>
+      )}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
