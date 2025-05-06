ecae8c0683b6ed7310d908a091b9f45c44cf9d66
Alexander Eide <atlexeide@gmail.com>
2025-05-06 19:10:42 +0200

    Add mobile catch

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,25 @@ export default function Home() {
   ];
 
   return (
-    <section id="catchphrase-cont">
-      <div>
-        <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
-        <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
-      </div>
-      <img id="mascot" src="mascot.png" alt="" />
-    </section>
+    <>
+      <section id="catchphrase-cont">
+        <div>
+          <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
+          <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
+        </div>
+        <img id="mascot" src="mascot.png" alt="" />
+      </section>
+
+      <section id="mobile-catch">
+        <img id="logo" src="pawlogo.png" alt="" />
+        <p>
+          We&apos;re working on the mobile version! <br /> <br />
+          For now, please visit on desktop to view the site ^^
+        </p>
+        <a href={import.meta.env.VITE_DISCORD_INVITE_LINK}>
+          <h4>Join us on discord for updates on development</h4>
+        </a>
+      </section>
+    </>
   );
 }
