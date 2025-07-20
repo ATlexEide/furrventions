4e3492832955ded82d488f45350cc4c5f26d0f46
Alexander Eide <atlexeide@gmail.com>
2025-07-20 16:05:35 +0200

    Add logic

--- src/utils/fetchLogo.js
+++ src/utils/fetchLogo.js
@@ -1,3 +1,9 @@
-export function fetchLogo(path) {
-  return;
+export function fetchLogo(supabase, name) {
+  console.log("retrieving ", name, " logo");
+  const { data } = supabase.storage
+    .from("convention-logos/logos")
+    .getPublicUrl(`${name}`);
+  console.log(data.publicUrl);
+
+  return data.publicUrl;
 }
