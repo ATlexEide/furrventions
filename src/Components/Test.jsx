452bb822277ae2c78dae9226b13bee9f5e7aa6c1
Alexander Eide <atlexeide@gmail.com>
2025-07-20 16:06:09 +0200

    Add test component

---
+++ src/Components/Test.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { uploadLogo } from "../utils/uploadLogo";
+
+export default function Test({ supabase }) {
+  const [file, setFile] = useState(null);
+
+  console.log("file:", file);
+  return (
+    <>
+      <button>test</button>
+      <input
+        type="file"
+        name="filepicker"
+        id="filepicker"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+      />
+      {file && (
+        <button
+          onClick={() => {
+            uploadLogo(supabase, "test with space", file);
+          }}
+        >
+          Upload
+        </button>
+      )}
+    </>
+  );
+}
