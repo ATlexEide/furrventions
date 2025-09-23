f5e5ebca1db3503f7c99cbd75f4444f6c5fd9e06
Alexander Eide <atlexeide@gmail.com>
2025-09-23 18:12:23 +0200

    Add wip alert component

---
+++ src/Components/Utilities/WipAlert.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function WipAlert() {
+  const [wipAccepted, setWipAccepted] = useState(true);
+
+  if (!localStorage.getItem("wipAlert")) {
+    localStorage.setItem("wipAlert", JSON.stringify({ accepted: false }));
+    setWipAccepted(false);
+  }
+
+  const wipAlert = JSON.parse(localStorage.getItem("wipAlert"));
+  if (!wipAlert.accepted) setWipAccepted(false);
+  console.log(wipAlert.accepted);
+
+  function acceptWipAlert() {
+    localStorage.setItem("wipAlert", JSON.stringify({ accepted: true }));
+    setWipAccepted(true);
+  }
+
+  return (
+    <>
+      {!wipAccepted && (
+        <div id="wipAlert">
+          <h2>ATTENTION</h2>
+          <p>
+            This is a project in development! <br />
+            There <em>will</em> be bugs and unintended behavior.
+            <br />
+            <br />
+            If you need help with something, please contact Velvet in our
+            discord server ^^
+          </p>
+          <hr />
+          <p>(This message will only be shown once)</p>
+          <button
+            onClick={() => {
+              acceptWipAlert();
+            }}
+          >
+            Proceed
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
