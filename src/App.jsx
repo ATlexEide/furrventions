ec1a9063240b819175e3c6987305e37f26f06185
Alexander Eide <atlexeide@gmail.com>
2025-09-23 18:12:37 +0200

    Refactor wip alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import WipAlert from "./Components/Utilities/WipAlert";
 
 export default function Home() {
-  const [wipAccepted, setWipAccepted] = useState(true);
-
-  useEffect(() => {
-    if (!localStorage.getItem("wipAlert")) {
-      localStorage.setItem("wipAlert", JSON.stringify({ accepted: false }));
-      setWipAccepted(false);
-    }
-
-    const wipAlert = JSON.parse(localStorage.getItem("wipAlert"));
-    if (!wipAlert.accepted) setWipAccepted(false);
-    console.log(wipAlert.accepted);
-  }, []);
-
-  function acceptWipAlert() {
-    localStorage.setItem("wipAlert", JSON.stringify({ accepted: true }));
-    setWipAccepted(true);
-  }
   // Taglines for display under landing page greeting
   const taglines = [
     "Your best friend in finding furry conventions!",
@@ -32,37 +13,14 @@ export default function Home() {
 
   return (
     <>
-      {!wipAccepted && (
-        <div id="wipAlert">
-          <h2>ATTENTION</h2>
-          <p>
-            This is a project in development! <br />
-            There <em>will</em> be bugs and unintended behavior.
-            <br />
-            <br />
-            If you need help with something, please contact Velvet in our
-            discord server ^^
-          </p>
-          <hr />
-          <p>(This message will only be shown once)</p>
-          <button
-            onClick={() => {
-              acceptWipAlert();
-            }}
-          >
-            Proceed
-          </button>
+      <WipAlert />
+      <section id="catchphrase-cont">
+        <div>
+          <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
+          <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
         </div>
-      )}
-      {wipAccepted && (
-        <section id="catchphrase-cont">
-          <div>
-            <h2 id="catchphrase">Welcome to {import.meta.env.VITE_APPNAME}</h2>
-            <h3>{taglines[Math.floor(Math.random() * taglines.length)]}</h3>
-          </div>
-          <img id="mascot" src="mascot.png" alt="" />
-        </section>
-      )}
+        <img id="mascot" src="mascot.png" alt="" />
+      </section>
 
       <section id="mobile-catch">
         <img id="logo" src="pawlogo.png" alt="" />
