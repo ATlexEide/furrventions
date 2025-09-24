0fcc02ff7d844e80fa5f39ae212b23d209c289ad
Alexander Eide <atlexeide@gmail.com>
2025-09-24 03:21:55 +0200

    Refactor inputs

--- src/Components/AddConventionForm/EventName.jsx
+++ src/Components/AddConventionForm/EventName.jsx
@@ -1,4 +1,23 @@
 import { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+import DescriptionIcon from "@mui/icons-material/Description";
+import BadgeIcon from "@mui/icons-material/Badge";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1
+});
 
 export default function EventName({
   setIsNotValid,
@@ -13,44 +32,65 @@ export default function EventName({
   return (
     <>
       <div className="input-container">
-        <label htmlFor="name">Event name*</label>
-        <input
+        <TextField
           id="name"
-          type="text"
           value={eventInfo.name}
+          label={
+            <span style={{ display: "flex", alignItems: "center", gap: 4 }}>
+              <BadgeIcon /> Event name *
+            </span>
+          }
+          variant="outlined"
           onChange={(e) => {
             setEventInfo({ ...eventInfo, name: e.target.value });
           }}
         />
+      </div>
 
-        <label htmlFor="description">Event description</label>
-        <textarea
+      <div className="input-container">
+        <TextField
           id="description"
           value={eventInfo.description}
+          label={
+            <span style={{ display: "flex", alignItems: "center", gap: 4 }}>
+              <DescriptionIcon /> Event description
+            </span>
+          }
+          variant="outlined"
           onChange={(e) => {
             setEventInfo({ ...eventInfo, description: e.target.value });
           }}
         />
+      </div>
 
-        <label htmlFor="eventLogoInput">Event Logo</label>
-        <input
-          type="file"
-          name="eventLogoInput"
-          id="eventLogoInput"
-          accept="image/png, image/gif, image/jpeg"
-          onChange={(e) => {
-            // File size limit (1MB * Limit)
-            if (e.target.files[0].size > 1048576 * 25) {
-              setError("filesize");
-            }
-            if (e.target.files.length) {
-              setEventInfo({
-                ...eventInfo,
-                logo: e.target.files[0]
-              });
-            }
-          }}
-        />
+      <div className="input-container">
+        <Button
+          fullWidth
+          component="label"
+          // loading
+          // loadingPosition="start"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<UploadFileIcon />}
+        >
+          {eventInfo.logo ? eventInfo.logo.name : "Event Logo"}
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(e) => {
+              // File size limit (1MB * Limit)
+              if (e.target.files[0].size > 1048576 * 25) {
+                setError("filesize");
+              }
+              if (e.target.files.length) {
+                setEventInfo({
+                  ...eventInfo,
+                  logo: e.target.files[0]
+                });
+              }
+            }}
+          />
+        </Button>
         {error && (
           <p>
             Image too big <br />
