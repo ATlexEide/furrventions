117d2c8b7e8e42cd59bca43035e6cee0dc271239
Alexander Eide <atlexeide@gmail.com>
2025-10-21 22:07:48 +0200

    Update list logic

--- src/Components/Utilities/VirtualizedList.jsx
+++ src/Components/Utilities/VirtualizedList.jsx
@@ -1,18 +1,32 @@
 import { List } from "react-window";
 
-const items = Array.from({ length: 10000 }, (_, index) => `Item ${index + 1}`);
-function Row({ index, style }) {
-  return <div style={style}>{items[index]}</div>;
+import "../../styles/VirtualizedList.css";
+
+// function RowComponent({ index, location, style }) {
+function RowComponent({ index, locations, style }) {
+  console.log(style);
+  console.log(locations[index]);
+  return (
+    <div
+      className="list-item"
+      onClick={() => console.log(locations[index])}
+      // className="flex items-center justify-between"
+      style={style}
+    >
+      <div className="list-item-text">{locations[index].display_name}</div>
+      {/* <div className="text-slate-500 text-xs">{`${index + 1} of ${ */}
+      <div>{`${index + 1} of ${locations.length}`}</div>
+    </div>
+  );
 }
-export default function VirtualizedList() {
+
+export default function VirtualizedList({ locations }) {
   return (
     <List
-      height={400} // Height of the container
-      itemCount={items.length} // Total number of items
-      itemSize={35} // Height of each row in pixels
-      width="100%" // Width of the container
-    >
-      {Row}
-    </List>
+      rowComponent={RowComponent}
+      rowCount={locations.length}
+      rowHeight={50}
+      rowProps={{ locations }}
+    />
   );
 }
