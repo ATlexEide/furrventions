9dd30e83d9dcef81a03c427733a7b8c743df84e7
Alexander Eide <atlexeide@gmail.com>
2025-10-31 03:28:06 +0100

    Add getHandler with with guard clauses

--- src/Components/Utilities/DateRangePicker.jsx
+++ src/Components/Utilities/DateRangePicker.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import "cally";
 import "../../styles/DateRangePicker.css";
 
-export default function DateRangePicker({ setValue, value }) {
+export default function DateRangePicker({ setEventInfo, eventInfo }) {
+  const [value, setValue] = useState("");
+
+  const getDateHandler = (e) => {
+    setValue(e.target.value);
+
+    const [date_start, date_end] = e.target.value.split("/");
+
+    if (date_start === date_end) {
+      setEventInfo({
+        ...eventInfo,
+        start_time: null,
+        end_time: null
+      });
+      return;
+    }
+
+    setEventInfo({
+      ...eventInfo,
+      start_time: date_start,
+      end_time: date_end
+    });
+  };
+
   return (
     <calendar-range
       id="calendar"
       months={2}
       value={value}
-      onchange={(event) => setValue(event.target.value)}
+      onchange={getDateHandler}
+      onClick={(e) => console.log(e.target.value)}
     >
       <div id="calendar-container">
         <calendar-month />
