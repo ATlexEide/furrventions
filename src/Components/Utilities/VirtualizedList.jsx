449359421e84cafe839ba628f19f4dae2db7364a
Alexander Eide <atlexeide@gmail.com>
2025-11-04 01:08:48 +0100

    Update list behavior to account for overflowing text

--- src/Components/Utilities/VirtualizedList.jsx
+++ src/Components/Utilities/VirtualizedList.jsx
@@ -1,6 +1,7 @@
 import { List } from "react-window";
 
 import "../../styles/VirtualizedList.css";
+import { useEffect, useRef, useState } from "react";
 
 // function RowComponent({ index, location, style }) {
 function RowComponent({
@@ -12,11 +13,22 @@ function RowComponent({
   setQuery,
   setResult
 }) {
-  console.log(style);
-  console.log(locations[index]);
+  const [isOverflowing, setIsOverFlowing] = useState(false);
+  const listItem = useRef(null);
+
+  useEffect(() => {
+    const text = listItem.current;
+    const textContainer = listItem.current.parentNode;
+
+    if (text.offsetWidth > textContainer.offsetWidth) {
+      console.log("overflow");
+      setIsOverFlowing(true);
+    }
+  }, []);
+
   return (
     <div
-      className="list-item"
+      className="border"
       onClick={() => {
         setEventInfo({
           ...eventInfo,
@@ -27,12 +39,13 @@ function RowComponent({
         setQuery(locations[index].display_name);
         setResult([]);
       }}
-      // className="flex items-center justify-between"
       style={style}
     >
-      <div className="list-item-text">{locations[index].display_name}</div>
-      {/* <div className="text-slate-500 text-xs">{`${index + 1} of ${ */}
-      <div>{`${index + 1} of ${locations.length}`}</div>
+      <div className={`list-item${isOverflowing ? " overflowing" : ""}`}>
+        <div ref={listItem} className="list-item-text">
+          <p>{locations[index].display_name}</p>
+        </div>
+      </div>
     </div>
   );
 }
@@ -46,9 +59,10 @@ export default function VirtualizedList({
 }) {
   return (
     <List
+      id="list"
       rowComponent={RowComponent}
       rowCount={locations.length}
-      rowHeight={50}
+      rowHeight={100}
       rowProps={{ locations, setEventInfo, eventInfo, setQuery, setResult }}
     />
   );
