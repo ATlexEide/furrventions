503187595e31d35344a35cf9c27c69e965a04146
Alexander Eide <atlexeide@gmail.com>
2025-11-05 19:27:06 +0100

    Add checkbox for free events

--- src/Components/AddConventionForm/EventTicketInfo.jsx
+++ src/Components/AddConventionForm/EventTicketInfo.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
+
 import EuroIcon from "@mui/icons-material/Euro";
 
 export default function EventTicketInfo({
@@ -7,17 +8,21 @@ export default function EventTicketInfo({
   eventInfo,
   setEventInfo
 }) {
+  const [isFree, setIsFree] = useState(eventInfo.price === 0 ? true : false);
+
   useEffect(() => {
-    if (eventInfo.price) setIsNotValid(false);
+    if (!isFree && !eventInfo.price) setIsNotValid(true);
+    else setIsNotValid(false);
   }, [eventInfo]);
 
   return (
     <>
       <div className="input-container">
         <TextField
+          disabled={isFree}
           type="number"
           id="name"
-          value={eventInfo.price ? eventInfo.price : ""}
+          value={eventInfo.price ? eventInfo.price : null}
           label={
             <span style={{ display: "flex", alignItems: "center", gap: 4 }}>
               <EuroIcon /> Ticket Price (EUR)*
@@ -25,10 +30,32 @@ export default function EventTicketInfo({
           }
           variant="outlined"
           onChange={(e) => {
-            setEventInfo({ ...eventInfo, price: e.target.value });
+            setEventInfo({ ...eventInfo, price: Number(e.target.value) });
           }}
         />
       </div>
+      {!eventInfo.price && (
+        <div className="input">
+          <label htmlFor="adult">Event is free</label>
+          <div>
+            <input
+              id="event-is-free-checkbox"
+              type="checkbox"
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setEventInfo({ ...eventInfo, price: 0 });
+                  setIsFree(true);
+                  setIsNotValid(false);
+                } else {
+                  setIsFree(false);
+                  setIsNotValid(true);
+                }
+              }}
+              checked={isFree}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 }
