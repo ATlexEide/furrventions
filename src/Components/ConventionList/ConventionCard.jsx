36e5734f3f877a40454b7e8e94f9389653e0dd52
Alexander Eide <atlexeide@gmail.com>
2026-02-26 04:14:13 +0100

    Update convention card Add notice for community added events Update description display

--- src/Components/ConventionList/ConventionCard.jsx
+++ src/Components/ConventionList/ConventionCard.jsx
@@ -52,62 +52,67 @@ export default function ConventionCard({ supabase, con, type }) {
   }
 
   return (
-    <article
-      onClick={() => {
-        navigate(`/conventions/${con.id}`);
-      }}
-      className={
-        type ? "convention-card" : "convention-card convention-info-card"
-      }
-    >
-      <figure className="convention-logo">
-        <img
-          src={
-            con.hasLogo
-              ? fetchLogo(con.id)
-              : "https://cydiwehmeqivbtceuupi.supabase.co/storage/v1/object/public/convention-images/pawlogo.png"
-          }
-          alt={`${con.name} logo`}
-        />
-      </figure>
-      <section className="convention-info">
-        {con.name && <h2 className="convention-name">{con.name}</h2>}
-        {con.location && <p>{con.location}</p>}
-        {con.spots_total && (
-          <p>
-            <span className="info-prefix">Total spots:</span> {con.spots_total}
-          </p>
-        )}
-        {con.spots_total && (
-          <>
+    <a href={`/conventions/${con.id}`}>
+      <article
+        onClick={() => {
+          navigate(`/conventions/${con.id}`);
+        }}
+        className={
+          type ? "convention-card" : "convention-card convention-info-card"
+        }
+      >
+        {!con.organizerID && <div className="notice">Community added</div>}
+        <figure className="convention-logo">
+          <img
+            src={
+              con.hasLogo
+                ? fetchLogo(con.id)
+                : "https://cydiwehmeqivbtceuupi.supabase.co/storage/v1/object/public/convention-images/pawlogo.png"
+            }
+            alt={`${con.name} logo`}
+          />
+        </figure>
+        <section className="convention-info">
+          {con.name && <h2 className="convention-name">{con.name}</h2>}
+          {con.location && <p>{con.location}</p>}
+          {con.spots_total && (
             <p>
-              <span className="info-prefix">Interested:</span>{" "}
-              {con.spots_taken ? con.spots_taken : 0}
+              <span className="info-prefix">Total spots:</span>{" "}
+              {con.spots_total}
             </p>
-            <p>
-              <span className="info-prefix">Spots left*:</span>{" "}
-              {con.spots_total - con.spots_taken}
-            </p>
-          </>
-        )}
-        {con.start_time && con.end_time && (
-          <div className="convention-dates">
-            <div>
-              <span className="info-prefix">Starts:</span>{" "}
-              <p>{formatDate(con.start_time)}</p>
-              <span className="info-prefix">Ends:</span>
-              <p>{formatDate(con.end_time)}</p>
+          )}
+          {con.spots_total && (
+            <>
+              <p>
+                <span className="info-prefix">Interested:</span>{" "}
+                {con.spots_taken ? con.spots_taken : 0}
+              </p>
+              <p>
+                <span className="info-prefix">Spots left*:</span>{" "}
+                {con.spots_total - con.spots_taken}
+              </p>
+            </>
+          )}
+          {con.start_time && con.end_time && (
+            <div className="convention-dates">
+              <div>
+                <span className="info-prefix">Starts:</span>{" "}
+                <p>{formatDate(con.start_time)}</p>
+                <span className="info-prefix">Ends:</span>
+                <p>{formatDate(con.end_time)}</p>
+              </div>
             </div>
-          </div>
-        )}
-      </section>
-      <section>
-        {con.description && (
-          <div>
-            <p className="convention-desc">{con.description}</p>
-          </div>
-        )}
-      </section>
-    </article>
+          )}
+        </section>
+        <section>
+          {con.description && (
+            <div className="convention-desc">
+              <h3>Event Description</h3>
+              <p>{con.description}</p>
+            </div>
+          )}
+        </section>
+      </article>
+    </a>
   );
 }
