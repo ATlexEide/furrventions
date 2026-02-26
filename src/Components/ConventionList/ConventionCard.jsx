f7b9aff36ab1649d1651ab6aa858731443e387a4
Alexander Eide <atlexeide@gmail.com>
2026-02-26 16:11:21 +0100

    Remove unused prop

--- src/Components/ConventionList/ConventionCard.jsx
+++ src/Components/ConventionList/ConventionCard.jsx
@@ -2,7 +2,7 @@ import { fetchLogo } from "../../utils/fetchLogo";
 import "../../styles/ConventionCard.css";
 import { useNavigate } from "react-router-dom";
 
-export default function ConventionCard({ supabase, con, type }) {
+export default function ConventionCard({ con, type }) {
   const navigate = useNavigate();
   const days = [
     "Sunday",
